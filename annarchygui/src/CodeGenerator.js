2245818dd74ddb038b6d119cad12f2e724dd2425
ignacio <ignacio.arias.t@usach.cl>
2024-11-27 17:40:37 -0300

    Escritura de modelos

--- annarchygui/src/CodeGenerator.js
+++ annarchygui/src/CodeGenerator.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 let lienzoRef = null;
+let code = '';
 
 /**
  * Establece la referencia al componente Lienzo.
@@ -55,10 +56,52 @@ export function getItemsFromLienzo(items) {
   return items;
 }
 
+/**
+ * Asegura que no haya espacios en los nombres y los reemplace con un guion.
+ * @param {string} name - Nombre a procesar.
+ * @returns {string} - Nombre procesado.
+ */
+export function formatName(name) {
+  return name.replace(/\s+/g, '-');
+}
+
+/**
+ * Traduce el listado de items en neuronas estilo ANNarchy y lo pone en code.
+ * @param {Array} items - Lista de elementos en el lienzo.
+ */
+export function generateANNarchyCode(items) {
+  const neurons = getNeurons(items);
+  code = neurons.map(neuron => {
+    const formattedName = formatName(neuron.name);
+    const params = Object.entries(neuron.parameters).map(([key, value]) => `\t\t${key}=${value}`).join(',\n');
+    const vars = Object.entries(neuron.variables).map(([key, value]) => `\t\t${key}=${value}`).join(',\n');
+    return `${formattedName} = Neuron(\n\tequations='${neuron.equation}',\n\tparameters={\n${params}\n\t},\n\tvariables={\n${vars}\n\t}\n)`;
+  }).join('\n\n');
+}
+
+/**
+ * Descarga el código generado como un archivo .py.
+ * @param {string} code - Código a descargar.
+ */
+export function downloadCodeAsFile(code) {
+  const blob = new Blob([code], { type: 'text/plain' });
+  const url = URL.createObjectURL(blob);
+  const a = document.createElement('a');
+  a.href = url;
+  a.download = 'generated_code.py';
+  document.body.appendChild(a);
+  a.click();
+  document.body.removeChild(a);
+  URL.revokeObjectURL(url);
+}
+
 const CodeGenerator = ({ items }) => {
   const handleTestGetItems = () => {
     const itemsList = getItemsFromLienzo(items);
-    console.log('Items from Lienzo:', itemsList);
+    console.log('Items:', itemsList);
+    generateANNarchyCode(itemsList);
+    console.log('Código tipo ANNarchy:', code);
+    downloadCodeAsFile(code);
   };
 
   return (
